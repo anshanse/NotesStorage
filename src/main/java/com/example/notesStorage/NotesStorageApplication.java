028499a4e828292aadce4a1630219ba787625c55
oleksiikrutko <krutko.av@gmail.com>
2021-11-10 12:01:51 +0200

    init

---
+++ src/main/java/com/example/notesStorage/NotesStorageApplication.java
@@ -0,0 +1,13 @@
+package com.example.notesStorage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotesStorageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotesStorageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/notesStorage/NotesStorageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.notesStorage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotesStorageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
