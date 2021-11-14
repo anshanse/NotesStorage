99458a6bb06d33aac7ecdbb224f82eda35e507d6
anshanse <shansea.s@gmail.com>
2021-11-14 20:20:10 +0200

    updated scripts

---
+++ src/main/resources/db/migration/V1__Init_DB.sql
@@ -0,0 +1,25 @@
+create table notes (
+	id varchar(36) not null,
+	access_type varchar(255),
+	message varchar(10000),
+	name varchar(255),
+	user_id varchar(36),
+	primary key (id)
+	);
+create table user_role (
+	user_id varchar(255) not null,
+	roles varchar(255)
+	);
+create table users (
+	id varchar(36) not null,
+	active boolean not null,
+	password varchar(255) not null,
+	username varchar(50) not null,
+	primary key (id)
+	);
+alter table if exists notes 
+	add constraint FKechaouoa6kus6k1dpix1u91c 
+	foreign key (user_id) references users;
+alter table if exists user_role 
+	add constraint FKj345gk1bovqvfame88rcx7yyx 
+	foreign key (user_id) references users;
