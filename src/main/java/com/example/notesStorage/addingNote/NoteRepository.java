afc38d22bf646becd4cca1db06e45fea64340739
evgen <allrent3@gmail.com>
2021-11-16 07:38:04 +0200

    refresh repositories

--- src/main/java/com/example/notesStorage/addingNote/NoteRepository.java
+++ src/main/java/com/example/notesStorage/addingNote/NoteRepository.java
@@ -4,13 +4,12 @@ import com.example.notesStorage.EntityRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface NoteRepository extends EntityRepository<Note, UUID> {
 
     @Query("SELECT u FROM #{#entityName} u WHERE u.name=?1")
-    List<Note> findByName(String name);
+    Iterable<Note> findByName(String name);
 
 }
