e96bf147f3777622249be5d2052f31a62999c2a0
evgen <allrent3@gmail.com>
2021-11-21 20:44:13 +0200

    add super_password_admin

--- src/main/resources/db/migration/V2__Add_admin.sql
+++ src/main/resources/db/migration/V2__Add_admin.sql
@@ -1,5 +1,5 @@
 insert into users (id, username, password, active)
-    values ('a81bc81b-dead-4e5d-abff-90865d1e13b1', 'admin', '123', true);
+    values ('a81bc81b-dead-4e5d-abff-90865d1e13b1', 'admin', '$2a$10$EuiP2S3jRW2bLCf2RQkWIeCzU1xGGAQXWUoT8nuiNpdaG2qVIozaK', true);
 
 insert into user_role (user_id, roles)
     values ('a81bc81b-dead-4e5d-abff-90865d1e13b1', 'USER'), ('a81bc81b-dead-4e5d-abff-90865d1e13b1', 'ADMIN');
