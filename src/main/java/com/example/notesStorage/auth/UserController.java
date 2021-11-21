debb240c526de363d7b5423b028382d3e0411c48
Riba33 <riba331845@gmail.com>
2021-11-21 23:08:39 +0200

    make it better14

--- src/main/java/com/example/notesStorage/auth/UserController.java
+++ src/main/java/com/example/notesStorage/auth/UserController.java
@@ -68,15 +68,9 @@ public class UserController {
     }
     @ExceptionHandler(ConstraintViolationException.class)
     ModelAndView onConstraintValidationException(ConstraintViolationException e, Model model) {
-        /*ValidationErrorResponse error = new ValidationErrorResponse();
-        for (ConstraintViolation violation : e.getConstraintViolations()) {
-            error.getViolations().add(
-                    new Violation(violation.getPropertyPath().toString(), violation.getMessage()));
-        }*/
         List<String> error = new ArrayList<>();
         Set<ConstraintViolation<?>> violations = e.getConstraintViolations();
         for (ConstraintViolation<?> violation : violations){
-            //error.append(violation.getMessage().concat("\\n"));
             error.add(violation.getMessage());
         }
         model.addAttribute("message",error);
