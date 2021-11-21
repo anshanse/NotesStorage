708a2c461d063f1db8d300cef5d697235a002518
anshanse <shansea.s@gmail.com>
2021-11-21 14:39:05 +0200

    update views

--- src/main/java/com/example/notesStorage/validator/ValidateUtils.java
+++ src/main/java/com/example/notesStorage/validator/ValidateUtils.java
@@ -1,5 +1,6 @@
 package com.example.notesStorage.validator;
 
+import org.springframework.beans.ConversionNotSupportedException;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
@@ -8,15 +9,16 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+import javax.validation.executable.ValidateOnExecution;
 import java.util.Map;
 import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
-
+@ValidateOnExecution
 public class ValidateUtils {
 
-    //@ResponseStatus(HttpStatus.BAD_REQUEST)
-    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(ConversionNotSupportedException.class)
     public static Map<String,String> getErrors(BindingResult bindingResult){
         Collector<FieldError, ?, Map<String, String>> collector = Collectors.toMap(
                 fieldError -> fieldError.getField(),
